ed761ec616095a1943130be1994d3311d8fbdf4c
Prashwitha Sanil <prashsanil03@gmail.com>
2022-06-29 21:44:38 +0530

    Exercise card implemented

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -14,7 +14,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
       const bodyPartsData = await 
       fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
       setBodyParts(['all', ...bodyPartsData]);
-    }
+    };
 
     fetchExercisesData();
   }, [])
@@ -24,7 +24,6 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
     if(search) {
       const exercisesData = await 
       fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
-      console.log(exercisesData);
 
       const searchedExercises = exercisesData.filter(
         (exercise) => exercise.name.toLowerCase().includes(search)
